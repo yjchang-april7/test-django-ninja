4d1982f5f367d0bf3b871d368af6ac4a5d556a61
yjchang-april7 <yjchang@april7.co.kr>
2023-03-02 17:25:10 +0900

    secret key is blank

--- test_ninja/settings.py
+++ test_ninja/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zclx_!*gzdd04@yc92cue2a3m%_at359n#k82ee*_0w_$&361o"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
